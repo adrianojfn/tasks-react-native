d528fa9a8b6f69e94c3d33894e232dc7baaf4fa1
Adriano França <adrianojfn@gmail.com>
2023-12-28 23:41:35 -0300

    Created the main application file

---
+++ src/screens/TaskList.js
@@ -0,0 +1,14 @@
+// This component represents the screen that contains the list of tasks
+
+import React, { Component } from "react";
+import { SafeAreaView, Text } from 'react-native'
+
+export default class TaskList extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text>TaskList</Text>
+            </SafeAreaView>
+        )
+    }
+}
